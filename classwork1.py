f8ed35d1a8451097505f8894699c2541320665f7
Yaroslaw_Kachmar <kachmaryar@gmail.com>
2019-08-16 09:57:33 +0300

    added tasks files

---
+++ classwork1.py
@@ -0,0 +1,42 @@
+# i=0
+# while i<100:
+#     if i%2==0:
+#         print(i)
+#     i+=1
+
+
+# for i in range(10):
+#     if i%2==0:
+#         print(i)
+
+
+# l=[6,4,2,7,2,3]
+# for i in l:
+#     if i%2 !=0:
+#         print('there is even number in your list')
+#         break
+
+
+# l=list(range(10))
+# i=0
+# while i<9:
+#     float (l[i])
+#     i +=1
+
+
+# f1=0
+# f2=1
+# i=0
+# n=10
+# while i<n:
+#     f_sum= f1+f2
+#     print(f_sum)
+#     f1=f2
+#     f2=f_sum
+#     i+=1
+
+
+l=['st','ri','ng']
+for i in l:
+    print(l[i])
+
---
+++ task2.py
@@ -0,0 +1,5 @@
+def reverse(st):
+    st=st.split()
+    return ' '.join(reversed(st))
+
+print(reverse("Oslo is a capital of Norway"))
