904a9d9919e0fbd2d5a48378426c7994b4fdbdb3
Yaroslaw_Kachmar <kachmaryar@gmail.com>
2019-08-22 13:05:02 +0300

    added classwork 7

--- hw6/classwork6.py
+++ hw6/classwork6.py
@@ -34,47 +34,47 @@
 #     return sum
 
 #########
-# def addition(*args):
-#     return(sum(args))
-# def substraction(*args):
-#     res=0
-#     for i in args:
-#         res-=i
-#     return res
-# def multiplication(*args):
-#     res=1
-#     for i in args:
-#         res*=i
-#     return res
-# def division(*args):
-#     res=1
-#     for i in args:
-#         res/=i
-#     return res
-# def num_input():
-#     values=input('Please enter numbers with space separation')
-#     list_of_val=[int(i) for i in values.split()]
-#     return list_of_val
+def addition(*args):
+    return(sum(args))
+def substraction(*args):
+    res=0
+    for i in args:
+        res-=i
+    return res
+def multiplication(*args):
+    res=1
+    for i in args:
+        res*=i
+    return res
+def division(*args):
+    res=1
+    for i in args:
+        res/=i
+    return res
+def num_input():
+    values=input('Please enter numbers with space separation')
+    list_of_val=[int(i) for i in values.split()]
+    return list_of_val
 
-# calc={'+':addition, '-':substraction, '*':multiplication, '/':division()}
+calc={'+':addition(), '-':substraction(), '*':multiplication(), '/':division()}
 
-# com=input('Enter "continue" or "exit"')
-# while com != 'exit':
+com=input('Enter "continue" or "exit"')
+while com != 'exit':
 
-#     list_of_values=num_input()
-#     inp_opr=input("Now please enter the operation to compute: ")
-#     if inp_opr == '+':
-#         print(calc['+'],*list_of_values)
-#     elif inp_opr == '-':
-#         print(calc['-'],*list_of_values)
-#     elif inp_opr == '*':
-#         print(calc['*'],*list_of_values)
-#     elif inp_opr == '/':
-#         print(calc['/'],*list_of_values)
-#     else: print('Error. Please get acquainted with the instruction')
-#     com=input('Enter "continue" or "exit"')
+    list_of_values=num_input()
+    inp_opr=input("Now please enter the operation to compute: ")
+    if inp_opr == '+':
+        print(calc['+'],*list_of_values)
+    elif inp_opr == '-':
+        print(calc['-'],*list_of_values)
+    elif inp_opr == '*':
+        print(calc['*'],*list_of_values)
+    elif inp_opr == '/':
+        print(calc['/'],*list_of_values)
+    else: print('Error. Please get acquainted with the instruction')
+    com=input('Enter "continue" or "exit"')
     
-# else: print('Thank you for choosing our product')
+else: print('Thank you for choosing our product')
 
 
 ########
--- hw7/classwork7.py
+++ hw7/classwork7.py
@@ -1,36 +1,42 @@
-# import pyowm
+import pyowm
 
-# owm = pyowm.OWM('203736c864231d379598ef7633bee76b')
+owm = pyowm.OWM('203736c864231d379598ef7633bee76b')
 
-# # Have a pro subscription? Then use:
-# # owm = pyowm.OWM(API_key='your-API-key', subscription_type='pro')
+c=input("Write your city:")
+observation = owm.weather_at_place(c)
+w = observation.get_weather()
 
-# # Search for current weather in Lviv (Ukraine)
-# observation = owm.weather_at_place('Lviv,UA')
-# w=str(input('Enter your town: ', end=''))
-# w = observation.get_weather()
-# print(w)                      # <Weather - reference time=2013-12-18 09:20,
-#                               # status=Clouds>
+temp = w.get_temperature('celsius')['temp'] # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+wind = w.get_wind()['speed'] # {'speed': 4.6, 'deg': 330}
+hum = w.get_humidity() # 87
 
-# # Weather details
-# w.get_wind()                  # {'speed': 4.6, 'deg': 330}
-# w.get_humidity()              # 87
-# w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+print('For now in', c ,'is', w.get_detailed_status() ) 
+print("The temperature in", c ,"is" , temp , "degrees Celsius.")
+print(" The speed of wind is", wind , "meters per second.")
+print(" The percentage of humidity in the air is" , hum , "% .")
 
 # # Search current weather observations in the surroundings of
 # # lat=22.57W, lon=43.12S (Rio de Janeiro, BR)
 # observation_list = owm.weather_around_coords(-22.57, -43.12)
 
-from random import *
+################
 
-ranval = random.randint(0,100)
-input_num=int(input('Input any number:'))
-if input_num < 1 and input_num > 100:
-    input_num=input('Error. Your number must be in range of 1 to 100! Please enter a number:')
-else:
-    while input_num != ranval:
-        if input_num < ranval:
-            print('Your number is less than ', ranval)
-        print('Your number is bigger than ', ranval)
-    else: print("You win the game")
+# import random
 
+# ranval = random.randint(0,100)
+# input_num=int(input('Input any number:'))
+# if input_num in range(0,100):
+#     while input_num != ranval:
+#         if input_num < ranval:
+#             print('Your number is less than ')
+#         print('Your number is greater than ')
+#         input_num=int(input('Input another number'))
+#     else: print("You win the game")
+# input_num=input('Error. Your number must be in range of 1 to 100! Please enter a number:')
+
+############
+
+from math import *
+def crc():
+    r = float(input("Input radius: "))
+    print("Square={}".format(PI * r**2))
