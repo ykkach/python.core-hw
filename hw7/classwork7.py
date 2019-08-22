248b16c78aafaaf1aa2f99f258b4529ff1827029
Yaroslaw_Kachmar <kachmaryar@gmail.com>
2019-08-22 15:01:14 +0300

    added hw7

--- hw7/classwork7.py
+++ hw7/classwork7.py
@@ -6,11 +6,12 @@
 # observation = owm.weather_at_place(c)
 # w = observation.get_weather()
 
+# time = observation.get_reception_time('iso')[:19]
 # temp = w.get_temperature('celsius')['temp'] # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
 # wind = w.get_wind()['speed'] # {'speed': 4.6, 'deg': 330}
 # hum = w.get_humidity() # 87
 
-# print('For now in', c ,'is', w.get_detailed_status() ) 
+# print('For', time, 'in', c ,'is', w.get_detailed_status() ) 
 # print("The temperature in", c ,"is" , temp , "degrees Celsius.")
 # print(" The speed of wind is", wind , "meters per second.")
 # print(" The percentage of humidity in the air is" , hum , "% .")
@@ -28,17 +29,35 @@
 # if input_num in range(0,100):
 #     while input_num != ranval:
 #         if input_num < ranval:
-#             print('Your number is less than ')
-#         print('Your number is greater than ')
+#             print('Your number is less than needed')
+#         else:
+#             print('Your number is greater than needed')
 #         input_num=int(input('Input another number'))
 #     else: print("You win the game")
-# input_num=input('Error. Your number must be in range of 1 to 100! Please enter a number:')
+# else:
+#     input_num=input('Error. Your number must be in range of 1 to 100! Please enter a number:')
 
 ############
 
 from math import *
+def rec():
+    a = float(input("a = "))
+    b = float(input("b = "))
+    print("S =", a*b)
+def trg():
+    a = float(input("a = "))
+    b = float(input("h = "))
+    print("S =", a*b*0.5)
 def crc():
     r = float(input("Input radius: "))
-    pi=3.14
-    print("S =", pi * r**2)
+    print("S =", math.pi * pow(r,2))
 crc()
+choice = input("1:rectangle, 2:triangle, 3:circle ")
+if choice == '1': 
+    rec()
+elif choice == '2':
+    trg()
+elif choice == '3':
+    crc()
+else:
+    print('Input error')
